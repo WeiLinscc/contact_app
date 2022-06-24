ab94ff11ab22a726dbb69d5ba9017eac450bf3d4
WeiLi <w0452569@nscc.ca>
2022-06-24 23:43:30 +0800

    Set up database and table

--- server/connection.js
+++ server/connection.js
@@ -2,12 +2,25 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: '127.0.0.1',
-  port: '8889',
+  port: '3306',
   user: 'root',
   password: 'root',
   database: 'nscctest'
 });
 
 connection.connect();
+
+connection.query(`CREATE TABLE IF NOT EXISTS contacts(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  image VARCHAR(255),
+  fist_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  phone_number VARCHAR(20) NOT NULL
+)  ENGINE=INNODB;`, err => {
+  if (err) {
+    console.error('err', err);
+  }
+});
 
 module.exports = connection;
