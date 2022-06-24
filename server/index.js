439a06ad147cc9ace5c34a23c908a14c8dc17cb6
WeiLi <w0452569@nscc.ca>
2022-06-25 02:49:54 +0800

    Import multer and upload image

--- server/index.js
+++ server/index.js
@@ -2,6 +2,29 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+file.originalname)
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
 
 const server = express();
 const port = 3000;
@@ -15,6 +38,12 @@ server.use(bodyParser.json());
 
 // routes
 server.get('/', (req, res) => res.send('Hello World!'));
+server.post('/upload', upload.single('file'), async (req, res, next) => {
+  if (req.file) {
+    const pathName = req.file.path;
+    res.sendStatus(200);
+  }
+});
 server.use('/', routes);
 
 server.listen(port, () =>
